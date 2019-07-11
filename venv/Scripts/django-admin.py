844b3e2c5291d9089dc0d1e9466b39e5ffd8b58b
RahulBhavihal1998 <rahulbhavihal.98@gmail.com>
2019-07-11 22:51:39 +0530

    My very first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\RAHUL BHAVIHAL\PycharmProjects\TechRover\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
